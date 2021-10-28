d0d2c76c9d157719aa877976600399c5853e20dc
Andreas Gamborg <s163939@student.dtu.dk>
2021-10-28 11:36:34 +0200

    recursive kalman filter

---
+++ SimulationDiscreteKalman.m
@@ -0,0 +1,124 @@
+%% Clean
+close all
+clear all
+clc
+
+%% Init
+Ts = 1/50;
+N = 10000;
+t = 0; % Start time
+T = [];
+
+%% Real Vessel
+state(12,1) = pi/2;
+O6 = Otter6(state);
+O6.UseProppeller = true;
+%% Model
+Model = 'Models/Primitive/otter3mtrx_lin.mat'
+load(Model);
+
+[F,G] = c2d(A,B,Ts);
+clear A B
+
+%% Controller
+Q = diag([1 1 10])*1000;        % Stateerror cost
+R = diag([0.1 10 1]);           % Input cost
+%N = diag([0 0 0]);
+[K,P,E] = dlqr(F,G,Q,R);
+
+%% Observer
+Cm = eye(3);                    % Measurement matrix
+Q = eye(3)*1000;                % Process noise
+R = eye(3)*0.01;                % Measurement noise
+[L,P,E] = dlqe(F,G,Cm,Q,R);
+
+Rvv = eye(3)*100;
+Rww = eye(3);
+P = eye(3);
+%% Reference
+r = [100 0 0]';
+r = [[3 0 0]' [1 0 110]' [0 -1 0]'];
+r = [[2 0 0]' [1 0 110]' [2 0 0]'];
+%r = [[3 0 0]' [5 0 0]' [3 0 0]'];
+rt = 1;
+
+%% Reference Scaling
+Fk = F-G*K;
+Cref = C*inv(eye(3)-Fk)*G;
+r = inv(Cref)*r;
+
+%% Init State
+nuhat = [0 0 0]';
+
+%% Main Loop
+disp('Running Simulation...')
+for it = 1:N
+    Mea = O6.getMeasurement();
+    ym = O6.measurementTransformation(Mea);
+    num = ym(1:3);
+    
+    if(it==N/4), rt = rt+1; end
+    if(it==3*N/4), rt = rt+1; end
+    tau = -K*nuhat + r(:,rt);
+    tau6 = [tau(1:2); zeros(3,1); tau(3)];
+    
+    % Input
+    O6.controlAllocation(tau6,nuhat);
+    O6.step(Ts);
+    % Observer
+    
+    
+    if(1)
+        S       = Cm*P*Cm' + Rvv;
+        kappa   = P*Cm'/S;
+        epsilon = num - Cm*nuhat;
+        nuhat   = nuhat + kappa*epsilon;
+        P       = P - kappa*S * kappa';
+        
+        nuhat   = F*nuhat + G*tau;
+        P       = F*P*F' + Rww;
+    else
+        nuhat = nuhat + L*(num - Cm*nuhat);
+        nuhat = F*nuhat + G*tau;
+        
+    end
+    
+    % Save
+    History.tau_r(:,it) = tau;
+    History.tau_a(:,it) = O6.Thrust([1 2 6]);
+    History.nu(:,it) = O6.State([1 2 6]);
+    History.num(:,it) = num;
+    History.nuhat(:,it) = nuhat;
+    
+    % Time update
+    T = [T t];
+    t = t+Ts;
+    
+end
+disp('Simulation done!')
+
+%% Plotting
+O6.plot(T)
+
+disp('Press any key to show estimates'), pause
+close all
+% u
+title = 'Linear Velocities';
+names = ["$u_m$ ", "$\hat{u}$", "$u$"];
+niceplot(T,toKnots([History.num(1,:); History.nuhat(1,:); History.nu(1,:)],'m/s'), names, title, [".","-","-"], ["time [s]", "[knot]"], 'northwest');
+
+% v
+title = 'Linear Velocities';
+names = ["$v_m$", "$\hat{v}$", "$v$"];
+niceplot(T,toKnots([History.num(2,:); History.nuhat(2,:); History.nu(2,:)],'m/s'), names, title, [".","-","-"], ["time [s]", "[knot]"], 'north');
+
+% r
+title = 'Angular Velocities';
+names = ["$r_m$", "$\hat{r}$", "$r$"];
+niceplot(T,[History.num(3,:); History.nuhat(3,:); History.nu(3,:)], names, title, [".","-","-"], ["time [s]", "[rad/s]"], 'northeast');
+
+% tau
+title = 'Thrust requested vs. applied';
+names = ["$\tau_{u,app}$","$\tau_{v,app}$","$\tau_{r,app}$","$\tau_{u,req}$","$\tau_{v,req}$","$\tau_{r,req}$"];
+niceplot(T, [History.tau_a; History.tau_r], names, title, ["-","--"], ["time [s]", "[N]"], 'southwest');
+
---
+++ SimulationWaypointsKalman.m
@@ -0,0 +1,194 @@
+%% Clean
+close all
+clear all
+clc
+
+%% Init
+Ts = 1/50;
+N = 20000;
+t = 0; % Start time
+T = [];
+
+%% Real Vessel
+state(12,1) = 0;
+O6 = Otter6(state);
+O6.UseProppeller = true;
+%% Model
+Model = 'Models/Primitive/otter3mtrx_lin.mat'
+load(Model);
+
+%% Controller
+Q = diag([1000 1000 10000]);    % Stateerror cost
+R = diag([0.1 10 1]);           % Input cost
+[K,P,E] = lqr(A,B,Q,R);
+
+%% Observer - Velocities
+Cm = eye(3);                    % Measurement matrix
+Q = diag([1000 1000 1000]);     % Process noise
+R = diag([0.1 0.1 0.1]);          % Measurement noise
+[L,P,E] = lqe(A,B,Cm,Q,R);
+
+%% Observer - Position
+Leta = diag([2 2 8]);
+Ld = diag([2 2 8]);
+
+%% Waypoints
+% WP = {WayPoint([20 0]',pi/2) WayPoint([20 20]' , pi) WayPoint([0 20]', -pi/2) WayPoint([0 0]', 0)};
+%WP = {WayPoint([-10 0]',pi)};
+
+
+WP = {  WayPoint([20 0]', -pi/4) WayPoint([20 -20]', -3*pi/4) WayPoint([0 -20]', 3*pi/4) WayPoint([0 0]', pi/2) ...
+       WayPoint([0 20]', 3*pi/4) WayPoint([-20 20]', -3*pi/4) WayPoint([-20 0]', -pi/4) WayPoint([0 0]', 0)    };
+
+WP{2}.setAccept(0.05,0.0175);
+WP{2}.precisionMode = true;
+iWP = 1;
+%% Init Observer State
+nuhat = [0 0 0]';
+etahat = [0 0 0]';
+dhat = [0 0 0]';
+
+
+%% Init Guidance System
+% Velocity
+umax = 3;              %[knot]
+umin = 0.5;              %[knot]
+k = 0.15;
+
+% Rotation
+k1 = 100;
+k2 = 20;
+k3 = -100;
+
+%% Main Loop
+disp('Running Simulation...')
+for it = 1:N
+    Mea = O6.getMeasurement();
+    ym = O6.measurementTransformation(Mea);
+    num = ym(1:3);
+    etam = ym(4:6);
+    
+    deltaG = WP{iWP}.pos-etahat(1:2);
+    if (norm(deltaG) < WP{iWP}.accept.distance) && (abs(WP{iWP}.heading - etahat(3)) < WP{iWP}.accept.angle)
+        iWP = iWP + 1;
+        if iWP > size(WP,2),disp('Final WayPoint reached'); break; end
+        deltaG = WP{iWP}.pos - etahat(1:2);
+    end
+    
+    psi = -etam(3);
+    R = [  cos(psi)   -sin(psi)
+        sin(psi)    cos(psi)   ];
+    deltaL = R * deltaG(1:2);
+    
+    bearG = atan2(deltaG(2),deltaG(1));
+    bearL = atan2(deltaL(2),deltaL(1));
+    
+    alpha = bearL;                  alpha = wrapToPi(alpha);
+    beta = bearG-WP{iWP}.heading;         beta = wrapToPi(beta);
+    theta = etahat(3)-WP{iWP}.heading;           theta = wrapToPi(theta);
+    
+    d = norm(deltaG(1:2,:));
+    speed = umax*umin/(umin+(umax-umin)*exp(-k*d));
+    
+    if WP{iWP}.precisionMode && norm(deltaG(1:2,:)) < 4% WP{iWP}.accept.distance
+        r = [speed*65*cos(alpha); speed*65*sin(alpha)/10; k3*theta];
+    else
+        r = [speed*65; 0; k1*alpha + k2*beta];
+    end
+    
+    tau = -K*nuhat + r;
+    % Input
+    Tr([1 2 6],1) = tau;
+    Ta = O6.controlAllocation(Tr,nuhat);
+    O6.step(Ts);
+    
+    
+    S_n       = C_m P C_m^T + R_{vv}      \\
+	\kappa    = P C_m^T S_n^{-1}          \\
+	\hat{y}   = C_m \hat{x}               \\
+	\epsilon  = y_m - \hat{y}             \\
+	nuhat     = \hat{x} + \kappa \epsilon \\
+	P         = P - \kappa S_n \kappa^T
+
+	x & = A x + B u       \\
+	P & = A P A' + R_{ww}
+    
+    % Observer
+    if it > N/2, dhat = dhat*0; end
+    dnuhat = A*nuhat + B*Ta([1 2 6]) + dhat + L*(num - Cm*nuhat);
+    ddhat = Ld*(num - Cm*nuhat);
+    nuhat = nuhat + Ts*dnuhat;    
+    dhat = ddhat + Ts*ddhat;
+
+    
+    etae = etam - etahat;
+    etae(3) = wrapToPi(etae(3));
+    detahat = Rot(etahat(3))*nuhat + Leta*etae;
+    etahat = etahat + Ts*detahat;
+    etahat(3) = wrapToPi(etahat(3));
+    
+    
+    
+    
+    
+    
+    
+    
+    % Save
+    History.ang(:,it) = [alpha;beta;theta];
+    History.course(:,it) = Mea.Course;
+    History.SOG(:,it) = Mea.SOG;
+    
+    History.num(:,it) = num;
+    History.nuhat(:,it) = nuhat;
+    History.nu(:,it) = O6.State([1 2 6]);
+    
+    History.etam(:,it) = etam;
+    History.etahat(:,it) = etahat;
+    History.eta(:,it) = O6.State([7 8 12]);
+    
+    % Time update
+    T = [T t];
+    t = t+Ts;
+    
+end
+disp('Simulation done!')
+
+%% Plotting
+close all
+O6.plot(T)
+title = 'Control angles';
+names = ["$\alpha$ ", "$\beta$", "$\theta$"];
+niceplot(T,rad2deg([History.ang]), names, title, ["-"], ["time [s]", "[deg]"], 'southeast');
+ytickformat('%.0f°')
+
+disp('Press any key to show estimates'), pause
+close all
+% u
+title = 'Linear Velocities';
+names = ["$u_m$ ", "$\hat{u}$", "$u$"];
+niceplot(T,toKnots([History.num(1,:); History.nuhat(1,:); History.nu(1,:)],'m/s'), names, title, [".","-","-"], ["time [s]", "[knot]"], 'northwest');
+
+title = 'Position';
+names = ["$x_m$", "$\hat{x}$", "$x$"];
+niceplot(T,[History.etam(1,:); History.etahat(1,:); History.eta(1,:)], names, title, [".","-","-"], ["time [s]", "[m]"], 'southwest');
+% v
+title = 'Linear Velocities';
+names = ["$v_m$", "$\hat{v}$", "$v$"];
+niceplot(T,toKnots([History.num(2,:); History.nuhat(2,:); History.nu(2,:)],'m/s'), names, title, [".","-","-"], ["time [s]", "[knot]"], 'north');
+
+title = 'Position';
+names = ["$y_m$", "$\hat{y}$", "$y$"];
+niceplot(T,[History.etam(2,:); History.etahat(2,:); History.eta(2,:)], names, title, [".","-","-"], ["time [s]", "[m]"], 'south');
+% r
+title = 'Angular Velocities';
+names = ["$r_m$", "$\hat{r}$", "$r$"];
+niceplot(T,[History.num(3,:); History.nuhat(3,:); History.nu(3,:)], names, title, [".","-","-"], ["time [s]", "[rad/s]"], 'northeast');
+
+title = 'Heading';
+names = ["$\phi_m$ ", "$\hat{\phi}$", "$\phi$"];
+niceplot(T,rad2deg([History.etam(3,:); History.etahat(3,:); History.eta(3,:)]), names, title,...
+    [".","-","-"], ["time [s]", "[deg]"], 'southeast');
+ytickformat('%.0f°')
+
+

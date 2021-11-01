4949ce8cb3b22694dd6e01a9bb1fbe84e1f93746
Andreas Gamborg <s163939@student.dtu.dk>
2021-11-01 13:07:09 +0100

    Guidance update

--- Simulation/SimulationGuidanceLookAhead.m
+++ Simulation/SimulationGuidanceLookAhead.m
@@ -32,41 +32,18 @@ R = diag([0.1 0.1 0.1]);          % Measurement noise
 Leta = diag([2 2 8]);
 Ld = diag([2 2 8]);
 
-%% Waypoints
-% WP = {WayPoint([20 0]',pi/2) WayPoint([20 20]' , pi) WayPoint([0 20]', -pi/2) WayPoint([0 0]', 0)};
-%WP = {WayPoint([-10 0]',pi)};
-
-   k = 30;
-   ad = 0.2;
-   aa = pi/36;
-WP = {  WayPoint([k 0]', -pi/4, ad, aa) WayPoint([k -k]', -3*pi/4, ad, aa)  ...
-        WayPoint([0 -k]', 3*pi/4, ad, aa) WayPoint([0 k]', 3*pi/4, ad, aa)  ...
-        WayPoint([-k k]', -3*pi/4, ad, aa) WayPoint([-k 0]', 0, ad, aa)     ...
-        WayPoint([0 0]', 0, ad, aa)    };
-   %WP = {  WayPoint([k 0]', -pi/4) WayPoint([k -k]', -pi/2)  };
-   %WP = {  WayPoint([20 5]', 0) WayPoint([k 0]', -pi/2) WayPoint([k -k]', -pi/2)  };
-
-% WP{2}.setAccept(0.05,0.0175);
-% WP{2}.precisionMode = true;
-iWP = 1;
+%% Course
+load('sinecurve.mat')
+closestPoint = 1;
+nP = length(P);
+lookaheaddist = 8;
 %% Init Observer State
 nuhat = [0 0 0]';
 etahat = [0 0 0]';
 dhat = [0 0 0]';
 
-
 %% Init Guidance System
-% Velocity
-umax = 2;              %[knot]
-umin = 0.1;              %[knot]
-k = 0.8;
-
-% Rotation
-k1 = 100;
-k2 = 20;
-k3 = 100;
-speed = 1;
-
+target = 1;
 %% Main Loop
 disp('Running Simulation...')
 for it = 1:N
@@ -75,59 +52,35 @@ for it = 1:N
     num = ym(1:3);
     etam = ym(4:6);
     
-    deltaG = WP{iWP}.pos-etahat(1:2);
-    if (norm(deltaG) < WP{iWP}.accept.distance) && (abs(WP{iWP}.heading - etahat(3)) < WP{iWP}.accept.angle)
-        iWP = iWP + 1;
-        if iWP > size(WP,2),disp('Final WayPoint reached'); break; end
-        deltaG = WP{iWP}.pos - etahat(1:2);
+    Pdiff = P-etahat(1:2);
+
+    dist = sqrt(sum(Pdiff.^2));
+    closestPoint = find(dist==min(dist));
+    
+    for p = closestPoint+1:nP
+       if dist(p) > lookaheaddist
+           target = p-1;
+           break;
+       end
     end
     
     psi = -etahat(3);
     R = [  cos(psi)   -sin(psi)
         sin(psi)    cos(psi)   ];
-    deltaL = R * deltaG(1:2);
+    deltaL = R * Pdiff(:,target);
+    alpha = atan2(deltaL(2),deltaL(1));
+    alpha = wrapToPi(alpha);
     
-    bearG = atan2(deltaG(2),deltaG(1));
-    bearL = atan2(deltaL(2),deltaL(1));
-    
-    alpha = bearL;                  alpha = wrapToPi(alpha);
-    beta = bearG-WP{iWP}.heading;         beta = wrapToPi(beta);
-    theta = WP{iWP}.heading-etahat(3);           theta = wrapToPi(theta);
-    
-    d = norm(deltaG(1:2,:));
-    %speed = umin+(umax-umin)* 1/(1+exp(-k*(d-5)));
+    r = [65; 0; 100*alpha];
 
-    
-    
-%     if WP{iWP}.precisionMode && norm(deltaG(1:2,:)) < 4% WP{iWP}.accept.distance
-%         r = [speed*65*cos(alpha); speed*65*sin(alpha)/10; k3*theta];
-%     else
-%         r = [speed*65; 0; k1*alpha + k2*beta];
-%     end
-    
-       
-    
-    speed = 0.1+0.9*umax*umin/(umin+(umax-umin)*exp(-k*(d-3)));
-    speed = umin + (umax-umin)./(1+exp(-k*(d-3)));
-    
-        d = d/speed;
-
-    k2 = 0.5*exp(-((d-8)/4).^2);
-    k3 = exp(-0.8*(d));
-    k1 = 1-k2-k3;
-
-    r = [speed*65; speed*65*k3*sin(alpha); 100*wrapToPi(k1*alpha + k2*beta + k3*theta)];
-    %r = [speed*65*cos(alpha); 0; 100*(k1*alpha + k2*beta + k3*theta)];
-
-    
     tau = -K*nuhat + r;
     % Input
     Tr([1 2 6],1) = tau;
     Ta = O6.controlAllocation(Tr,nuhat);
     O6.step(Ts);
     
+    
     % Observer
-%     if it > N/2, dhat = dhat*0; end
     dnuhat = A*nuhat + B*Ta([1 2 6]) + dhat + L*(num - Cm*nuhat);
     ddhat = Ld*(num - Cm*nuhat);
     nuhat = nuhat + Ts*dnuhat;    
@@ -142,8 +95,7 @@ for it = 1:N
     
     
     % Save
-    History.ang(:,it) = [alpha;beta;theta];
-    History.gains(:,it)  = [k1;k2;k3;d];
+    History.ang(:,it) = [alpha];
     History.course(:,it) = Mea.Course;
     History.SOG(:,it) = Mea.SOG;
     
@@ -164,15 +116,11 @@ disp('Simulation done!')
 
 %% Plotting
 close all
-O6.plot(T,WP)
+O6.plot(T)
 title = 'Control angles';
-names = ["$\alpha$ ", "$\beta$", "$\theta$"];
+names = ["$\alpha$ "];
 niceplot(T,rad2deg([History.ang]), names, title, ["-"], ["time [s]", "[deg]"], 'southeast');
 ytickformat('%.0f°')
-
-title = 'gain parameter';
-names = ["$k_1$","$k_2$","$k_3$","$d$"];
-niceplot(T,[History.gains], names, title, ["-"], ["time [s]", " "], 'southwest');
 
 disp('Press any key to show estimates'), pause
 close all
